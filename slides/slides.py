0e79d75b7a4538473282be14ffe759f300798340
Guillaume Anciaux <guillaume.anciaux@epfl.ch>
2023-05-10 13:11:32 +0200

    adding lectures

--- slides/slides.py
+++ slides/slides.py
@@ -6,8 +6,8 @@ lectures = ["0_Introduction_class",
             "2_Course_Fracture_Mechanics",
             "3_Course_Fragmentation",
             "4_Course_fe_dynamics",
-            # "5_1_Course_Comp_Frac_Cohesive",
-            # "5_2_Course_Comp_Frac_Phase_Field",
+            "5_1_Course_Comp_Frac_Cohesive",
+            "5_2_Course_Comp_Frac_Phase_Field",
             # "6_Course_HPC_Fragmentation"
             ]
 
